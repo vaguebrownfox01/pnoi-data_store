92a0fed0dbf18d76d732bdc067c88633ac56fc29
vaguebrownfox <foxjournal.nsfw@gmail.com>
2022-09-23 17:34:10 +0530

    authentication

---
+++ src/app/firebase/client/auth.js
@@ -0,0 +1,18 @@
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { AUTH_DOM } from "../creds/.setup";
+import { au } from "../creds/client";
+
+export const firebaseAdminLogin = async (uid, passkey) => {
+	const email = `${uid}@${AUTH_DOM}`;
+	const res = await signInWithEmailAndPassword(au, email, passkey).catch(
+		(e) => console.log(e)
+	);
+
+	return !!res;
+};
+
+export const firebaseAdminLogout = async () => {
+	const res = await signOut(au).catch((e) => console.log(e));
+
+	return res;
+};
--- src/app/pieces/MainPanel.js
+++ src/app/pieces/MainPanel.js
@@ -1,10 +1,30 @@
+import { Button, Fab, Stack } from "@mui/material";
 import * as React from "react";
+import useAuth from "../hooks/useAuth";
+import AuthModal from "./AuthModal";
 import Sections from "./Sections";
+import LockIcon from "@mui/icons-material/Lock";
 
 const MainPanel = React.memo(function MainPanel() {
+	const [user, handleAuth, handleLogout] = useAuth();
 	return (
 		<>
-			<Sections />
+			{!user ? (
+				<AuthModal open={!!user} {...{ handleAuth }} />
+			) : (
+				<Sections />
+			)}
+			<Stack direction="row-reverse">
+				<Fab
+					sx={{ mt: 2 }}
+					size="small"
+					color="grey"
+					aria-label="logout"
+					onClick={handleLogout}
+				>
+					<LockIcon />
+				</Fab>
+			</Stack>
 		</>
 	);
 });
