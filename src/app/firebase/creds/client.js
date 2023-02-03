85b6117ddf60a770285ff317e48e68eb8f149251
vaguebrownfox <foxjournal.nsfw@gmail.com>
2023-02-03 19:40:49 +0530

    message

--- src/app/firebase/creds/client.js
+++ src/app/firebase/creds/client.js
@@ -2,7 +2,17 @@ const { getFirestore } = require("firebase/firestore");
 const { getStorage } = require("firebase/storage");
 const { initializeApp } = require("firebase/app");
 const { getAuth } = require("firebase/auth");
-const config = require("./.client-pnoistor.json");
+// const config = require("./.client-pnoistor.json");
+
+let config = {
+	apiKey: process.env.APIKEY,
+	authDomain: process.env.AUTHDOMAIN,
+	projectId: process.env.PROJECTID,
+	storageBucket: process.env.STORAGEBUCKET,
+	messagingSenderId: process.env.MESSAGINGSENDERID,
+	appId: process.env.APPID,
+	measurementId: process.env.MEASUREMENID,
+};
 
 const app = initializeApp(config);
 
