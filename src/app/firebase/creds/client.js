f559a0bb81a3e74ce93e5c11ab99f6215861bae0
vaguebrownfox <foxjournal.nsfw@gmail.com>
2023-02-05 10:55:11 +0530

    no process

--- src/app/firebase/creds/client.js
+++ src/app/firebase/creds/client.js
@@ -4,16 +4,6 @@ const { initializeApp } = require("firebase/app");
 const { getAuth } = require("firebase/auth");
 const config = require("./client-pnoistor.json");
 
-// let config = {
-// 	apiKey: process.env.APIKEY,
-// 	authDomain: process.env.AUTHDOMAIN,
-// 	projectId: process.env.PROJECTID,
-// 	storageBucket: process.env.STORAGEBUCKET,
-// 	messagingSenderId: process.env.MESSAGINGSENDERID,
-// 	appId: process.env.APPID,
-// 	measurementId: process.env.MEASUREMENID,
-// };
-
 const app = initializeApp(config);
 
 export const db = getFirestore(app);
